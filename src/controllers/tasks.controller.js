c1fc1af225ca133cb360948e1aed4722fd653533
Alexandra Quintero <alexandra.quintero@hotmail.com>
2022-02-22 16:33:43 -0500

    my first commit

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,58 @@
+import Task from "../models/Task";
+
+export const renderTasks = async (req, res) => {
+  const tasks = await Task.find().lean();
+
+  res.render("index", { tasks: tasks });
+};
+
+export const createTask = async (req, res) => {
+  try {
+    const task = Task(req.body);
+
+    await task.save();
+
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const renderTaskEdit = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+    res.render("edit", { task: task });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const editTask = async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndUpdate(id, req.body);
+  res.redirect("/");
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const taskToggleDone = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const task = await Task.findById(id);
+    task.done = !task.done;
+    await task.save();
+
+    res.redirect("/");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
---
+++ src/routes/index.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import {
+  renderTasks,
+  createTask,
+  renderTaskEdit,
+  editTask,
+  deleteTask,
+  taskToggleDone,
+} from "../controllers/tasks.controller";
+
+const router = Router();
+
+router.get("/", renderTasks);
+
+router.post("/tasks/add", createTask);
+
+router.get("/tasks/:id/edit", renderTaskEdit);
+
+router.post("/tasks/:id/edit", editTask);
+
+router.get("/tasks/:id/delete", deleteTask);
+
+router.get("/tasks/:id/toogleDone", taskToggleDone);
+
+export default router;
